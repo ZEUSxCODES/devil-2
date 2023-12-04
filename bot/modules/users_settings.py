226f62db723ab6e4218515c61ba502e1a23081e1
Darkksoul <139141380+Darkksoul@users.noreply.github.com>
2023-12-04 17:24:14 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -165,7 +165,7 @@ async def update_user_settings(query, key=None, edit_type=None, edit_mode=None, 
     user_id = query.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
-        thumbnail = 'https://te.legra.ph/file/94bc067909902a02eaf20.jpg'
+        thumbnail = 'https://te.legra.ph/file/b700bbbf7329b04f3673a.jpg'
     await editMessage(query.message, msg, button, thumbnail)
 
 
@@ -175,7 +175,7 @@ async def user_settings(client, message):
     user_id = message.from_user.id
     thumbnail = f"Thumbnails/{user_id}.jpg"
     if not ospath.exists(thumbnail):
-        thumbnail = 'https://te.legra.ph/file/94bc067909902a02eaf20.jpg'
+        thumbnail = 'https://te.legra.ph/file/b700bbbf7329b04f3673a.jpg'
     x = await sendMessage(message, msg, button, thumbnail)
     await five_minute_del(message)
     await deleteMessage(x)
